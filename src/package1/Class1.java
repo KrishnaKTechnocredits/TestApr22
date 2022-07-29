0154a262a58c769e24bcf9439e5ae87f9d9e6a4b
kanani.krishna <kanani.krishna>
2022-07-29 08:14:58 +0530

    Main method change

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -2,7 +2,10 @@ package package1;
 
 public class Class1 {
 
-	public static void main(String[] args) {
-		System.out.println("Project upload");
+	public static void main(String[] args) 
+	{
+		System.out.println("Project upload branch A");
+		System.out.println("Project upload........A");
+		System.out.println("Project upload--------A");
 	}
 }
