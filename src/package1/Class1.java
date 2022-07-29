b24420178adb3e13e79cfeebc2673e46735d2cb1
kanani.krishna <kanani.krishna>
2022-07-29 09:07:28 +0530

    Branch B1 changes

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -2,12 +2,10 @@ package package1;
 
 public class Class1 {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.println("Project upload branch A");
 		System.out.println("Project upload........A");
 		System.out.println("Project upload--------A");
 		System.out.println("Project upload branch B");
-		System.out.println("Project upload........B");
 	}
 }
