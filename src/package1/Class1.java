dd86e7f4ac40ae9e7cbaec3cbc12591f98da27e3
kanani.krishna <kanani.krishna>
2022-07-30 10:05:03 +0530

    Removed code

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -7,7 +7,5 @@ public class Class1 {
 		System.out.println("Project upload branch A");
 		System.out.println("Project upload........A");
 		System.out.println("Project upload--------A");
-		System.out.println("Project upload branch B");
-		System.out.println("Project upload........B");
 	}
 }
